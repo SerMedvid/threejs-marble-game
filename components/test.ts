b4ab1a51395f5b981e67243f4a464d33dad6e310
Sergii Medvid <mr.medvid@gmail.com>
2023-08-11 19:57:49 +0300

    first commit

---
+++ components/test.ts
@@ -0,0 +1,15 @@
+const getServerSideProps = async () => {
+	const data = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+	const json: { title: string } = await data.json();
+	return {
+		props: {
+			json,
+		},
+	};
+};
+
+type InferPropsFromServerSideFunction<T> = T extends () => Promise<{
+	props: infer R;
+}>
+	? R
+	: never;
